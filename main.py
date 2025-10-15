9c08d0c174b627a4db152f39e04bb6a8d1502540
Yuri Freitas <yuriftq@gmail.com>
2025-10-14 21:21:25 -0300

    Upload: Parte3, criando excel NotasFiscais.xlsx, usando o pandas para criar um df e depois salvar em xslx

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import xmltodict
 import os
 import json
-
+import pandas as pd
 
 # Pegar informações 
-def pegar_infos(arq):
+def pegar_infos(arq, valores):
     # print(f"Pegou as informações da nota {arq}")
     # with open(f"nfs/{arquivo}", 'r') as arquivo_xml:
     with open(f"nfs/{arq}", 'rb') as arquivo_xml:
@@ -27,24 +27,44 @@ def pegar_infos(arq):
             else:
                 peso_bruto = "Não informado na nf"
 
-            print(f"""
-                Número da nf {arquivo_xml.name}: {numero_nf}
-                Empresa Emissora: {empresa_emissora}
-                Cliente: {nome_cliente}
-                Endereço Cliente: {end_cliente}
-                Peso Bruto da venda: {peso_bruto}
-                """)
+            # print(f"""
+            #     Número da nf {arquivo_xml.name}: {numero_nf}
+            #     Empresa Emissora: {empresa_emissora}
+            #     Cliente: {nome_cliente}
+            #     Endereço Cliente: {end_cliente}
+            #     Peso Bruto da venda: {peso_bruto}
+            #     """)
+            valores.append([numero_nf, empresa_emissora, nome_cliente, end_cliente, peso_bruto])
+            
             print(f"ARQUIVO LIDO COM SUCESSO: {arq}")
         except Exception as e:
             print('Erro:', e)
             print(json.dumps(dic_arq, indent=4))
 
 
+colunas = ["Numero Nota", "Empresa Emissora", "Nome Cliente", "Endereço", "Peso_Bruto"]
+
+
 # Pegando o nome dos arquivos
 lista_arquivos = os.listdir("nfs")
 # print(arquivos)
 
+# Lista iniciada para pegar os valores das linhas
+valores = []
 for arquivo in lista_arquivos:
-    pegar_infos(arquivo)
+    pegar_infos(arquivo, valores)
     # Debug
     # break
+
+# criando tabela
+tabela = pd.DataFrame(columns=colunas, data=valores)
+
+# Debug
+# print(tabela)
+
+# Criando excel
+tabela.to_excel("NotasFiscais.xlsx", index=False)
+
+    
+    
+    
